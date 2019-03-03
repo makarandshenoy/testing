b97e0408b5e13a52d1326f1ffa18d08f8039f013
makarandshenoy <48171918+makarandshenoy@users.noreply.github.com>
2019-03-03 19:28:23 +0530

    Update import test.tf

--- import test.tf
+++ import test.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
 
 resource "aws_security_group" "makkusg" {
   name        = "makku_sg"
